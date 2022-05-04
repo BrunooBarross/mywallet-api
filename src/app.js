76c904953fc8c1b13cece15dfb1fd968ae0b5018
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-05-03 22:26:26 -0300

    feat: Criando conexao com o banco de dados, criando função post para cadastro de usuários, assim como realizando as devidas validações (joi, senhas, criptografia)

--- src/app.js
+++ src/app.js
@@ -2,10 +2,56 @@ import express, {json} from "express";
 import cors from "cors";
 import chalk from "chalk";
 import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+import joi from "joi";
+import bcrypt from 'bcrypt';
+
 dotenv.config();
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+const dbCarteira = mongoClient.db("carteira");
 
 const app = express();
 app.use(cors());
 app.use(json());
 
-app.listen(5000, console.log(chalk.bold.yellow("Servidor rodando na porta 5000")));
+app.post("/cadastrar", async (req, res) => {
+    const {email , senha, senhaverificada} = req.body;
+    if(senha !== senhaverificada){
+        console.log('senhas diferentes')
+        res.sendStatus(406);
+        return;
+    }
+    const schema = joi.object({
+        email: joi.string().required(),
+        senha: joi.string().required(),
+        senhaverificada: joi.string().required()
+    })
+    const validacao = schema.validate(req.body, { abortEarly: false })
+
+    if (validacao.error) {
+        console.log(chalk.bold.red("Erro joi cadastro"), validacao.error.details)
+        res.sendStatus(422);
+        return;
+    }
+
+    const senhaCriptografada = bcrypt.hashSync(senha, 10);
+    try {
+        await mongoClient.connect();
+        const usuariosCollection = dbCarteira.collection('usuarios');
+        const temUsuario = await usuariosCollection.findOne({ email: email });
+        if(temUsuario){
+            console.log(chalk.bold.red("O email já existe"));
+            res.status(409).send("Email já cadastrado");
+            return;
+        }
+        await usuariosCollection.insertOne({email, senha: senhaCriptografada});
+        res.sendStatus(201);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+
+    }
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(chalk.bold.blue(` Servidor rodando na porta ${port}`)));
